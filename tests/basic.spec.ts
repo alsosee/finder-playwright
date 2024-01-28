272f8378f68039c841cd0a3d48d4c0f3c463d91d
Konstantin Chukhlomin <mail@chuhlomin.com>
2024-01-27 20:29:45 -0500

    feat: add "remember view choice" test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -8,10 +8,6 @@ test("has title and Games folder", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/Also, see/);
 
-  // Body data-view attribute
-  const body = expect(page.locator("xpath=//body"));
-  await body.toHaveAttribute("data-view", "icons");
-
   // Games folder link
   const link = expect(page.getByRole("menuitem", { name: "Games" }));
   await link.toBeVisible();
@@ -24,4 +20,34 @@ test("has title and Games folder", async ({ page }) => {
       .getByRole("menubar", { name: "breadcrumbs" })
       .filter({ hasText: "Home" }),
   ).toBeVisible();
+});
+
+test("remember view choice", async ({ page }) => {
+  await page.goto("/");
+
+  // Body data-view attribute
+  const body = expect(page.locator("xpath=//body"));
+  await body.toHaveAttribute("data-view", "icons");
+
+  // Switch to list view
+  const buttonList = page
+    .getByRole("menuitem")
+    .filter({ has: page.locator("xpath=//input[@value='list']") });
+  await expect(buttonList).toBeVisible();
+  await buttonList.click();
+  await body.toHaveAttribute("data-view", "list");
+
+  await page.reload();
+  await body.toHaveAttribute("data-view", "list");
+
+  // Switch to columns view
+  const buttonColumns = page
+    .getByRole("menuitem")
+    .filter({ has: page.locator("xpath=//input[@value='columns']") });
+  await expect(buttonColumns).toBeVisible();
+  await buttonColumns.click();
+  await body.toHaveAttribute("data-view", "columns");
+
+  await page.reload();
+  await body.toHaveAttribute("data-view", "columns");
 });
